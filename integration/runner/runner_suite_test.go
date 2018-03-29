ca9cc034adf0f096b18edf2169558e9e0c8bc295
Matthew Sykes <sykesmat@us.ibm.com>
2018-03-29 13:39:18 -0400

    [FAB-9232] Create cryptogen runner
    Begin to build the infrastructure necessary to run end to end
    integration tests with `go test`.
    
    Change-Id: I675df4ff0b91f45e99c41137e04c4cc937ef8cc5
    Signed-off-by: Latitia M Haskins <latitia.haskins@gmail.com>
    Signed-off-by: Matthew Sykes <sykesmat@us.ibm.com>

--- integration/runner/runner_suite_test.go
+++ integration/runner/runner_suite_test.go
@@ -7,13 +7,32 @@ SPDX-License-Identifier: Apache-2.0
 package runner_test
 
 import (
+	"os"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gexec"
 
 	"testing"
 )
+
+var cryptogenPath string
 
 func TestRunner(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Runner Suite")
 }
+
+var _ = SynchronizedBeforeSuite(func() []byte {
+	cryptogenPath, err := gexec.Build("github.com/hyperledger/fabric/common/tools/cryptogen")
+	Expect(err).NotTo(HaveOccurred())
+
+	return []byte(cryptogenPath)
+}, func(data []byte) {
+	cryptogenPath = string(data)
+})
+
+var _ = SynchronizedAfterSuite(func() {
+}, func() {
+	os.Remove(cryptogenPath)
+})
